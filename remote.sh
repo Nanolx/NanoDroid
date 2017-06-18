4a57efb85f5ca7fae19d641878e3e9ea46a68cdb
Christopher Roy Bratusek <nano@jpberlin.de>
2017-06-18 10:57:52 +0200

    use simplified mount-code for magisk.img mounting (from Magisk v13 beta)

--- remote.sh
+++ remote.sh
@@ -1,27 +1,27 @@
 #!/sbin/sh
 
 is_mounted() {
-  if [ ! -z "$2" ]; then
-    cat /proc/mounts | grep $1 | grep $2, >/dev/null
-  else
-    cat /proc/mounts | grep $1 >/dev/null
-  fi
-  return $?
+	if [ ! -z "$2" ]; then
+		cat /proc/mounts | grep $1 | grep $2, >/dev/null
+	else
+		cat /proc/mounts | grep $1 >/dev/null
+	fi
+	return $?
 }
 
 request_size_check() {
-  reqSizeM=`unzip -l "$1" 2>/dev/null | tail -n 1 | awk '{ print $1 }'`
-  reqSizeM=$((reqSizeM / 1048576 + 1))
+	reqSizeM=`unzip -l "$1" 2>/dev/null | tail -n 1 | awk '{ print $1 }'`
+	reqSizeM=$((reqSizeM / 1048576 + 1))
 }
 
 image_size_check() {
-  e2fsck -yf $1
-  curBlocks=`e2fsck -n $1 2>/dev/null | grep $1 | cut -d, -f3 | cut -d\  -f2`;
-  curUsedM=`echo "$curBlocks" | cut -d/ -f1`
-  curSizeM=`echo "$curBlocks" | cut -d/ -f1`
-  curFreeM=$(((curSizeM - curUsedM) * 4 / 1024))
-  curUsedM=$((curUsedM * 4 / 1024 + 1))
-  curSizeM=$((curSizeM * 4 / 1024))
+	e2fsck -yf $1
+	curBlocks=`e2fsck -n $1 2>/dev/null | grep $1 | cut -d, -f3 | cut -d\  -f2`;
+	curUsedM=`echo "$curBlocks" | cut -d/ -f1`
+	curSizeM=`echo "$curBlocks" | cut -d/ -f1`
+	curFreeM=$(((curSizeM - curUsedM) * 4 / 1024))
+	curUsedM=$((curUsedM * 4 / 1024 + 1))
+	curSizeM=$((curSizeM * 4 / 1024))
 }
 
 grow_magisk_img () {
@@ -42,36 +42,27 @@ shrink_magisk_img () {
 }
 
 mount_image() {
-  if [ ! -d "$2" ]; then
-    mount -o rw,remount rootfs /
-    mkdir -p $2 2>/dev/null
-    [ ! -d "$2" ] && return 1
-  fi
-  if (! is_mounted $2); then
-    LOOPDEVICE=
-    for LOOP in 0 1 2 3 4 5 6 7; do
-      if (! is_mounted $2); then
-        LOOPDEVICE=/dev/block/loop$LOOP
-        if [ ! -e "$LOOPDEVICE" ]; then
-          mknod $LOOPDEVICE b 7 $LOOP
-        fi
-        losetup $LOOPDEVICE $1
-        if [ "$?" -eq "0" ]; then
-          mount -t ext4 -o loop $LOOPDEVICE $2
-          if (! is_mounted $2); then
-            /system/bin/toolbox mount -t ext4 -o loop $LOOPDEVICE $2
-          fi
-          if (! is_mounted $2); then
-            /system/bin/toybox mount -t ext4 -o loop $LOOPDEVICE $2
-          fi
-        fi
-        if (is_mounted $2); then
-          echo "$LOOPDEVICE" > /tmp/loopdevice
-          break;
-        fi
-      fi
-    done
-  fi
+	if [ ! -d "$2" ]; then
+		mount -o rw,remount rootfs /
+		mkdir -p "$2" 2>/dev/null
+		$BOOTMODE && mount -o ro,remount rootfs /
+		[ ! -d "$2" ] && return 1
+	fi
+
+	if ! is_mounted "$2"; then
+		LOOPDEVICE=
+		for LOOP in 0 1 2 3 4 5 6 7; do
+			if ! is_mounted "$2"; then
+				LOOPDEVICE=/dev/block/loop$LOOP
+				[ -e $LOOPDEVICE ] || mknod $LOOPDEVICE b 7 $LOOP 2>/dev/null
+				losetup $LOOPDEVICE "$1" && mount -t ext4 -o loop $LOOPDEVICE "$2"
+				if is_mounted "$2"; then
+					echo "$LOOPDEVICE" > /tmp/loopdevice
+					break;
+				fi
+			fi
+		done
+	fi
 }
 
 mount_magisk () {
