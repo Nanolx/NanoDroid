c2ad31de67d3bca17ba65d6e37ea2c1fc9a9cee1
Christopher Roy Bratusek <nano@jpberlin.de>
2017-11-24 21:37:58 +0100

    bump version to 14.8 in-dev

--- patcher/dexpatcher/75-nanomodpatcher.sh
+++ patcher/dexpatcher/75-nanomodpatcher.sh
@@ -390,7 +390,7 @@ main () {
 	ui_print " "
 	ui_print "*******************************"
 	ui_print "   NanoMod Framework Patcher   "
-	ui_print "     addon.d 14.7.20171115     "
+	ui_print "     addon.d 14.8.99999999     "
 	ui_print "       created by @Nanolx      "
 	ui_print " Utilizing DexPatcher @lanchon "
 	ui_print "*******************************"
