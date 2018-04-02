03384cdfed8410a00d9c9b616fb05239a1730c42
Christopher Roy Bratusek <nano@jpberlin.de>
2018-04-02 19:50:20 +0200

    bump version to 16.5.20180402

--- patcher/dexpatcher/75-nanodroidpatcher.sh
+++ patcher/dexpatcher/75-nanodroidpatcher.sh
@@ -26,7 +26,7 @@ NanoDroidPatcher () {
 	ui_print " "
 	ui_print "*******************************"
 	ui_print "   NanoDroid Framework Patcher   "
-	ui_print "     addon.d 16.5.99999999     "
+	ui_print "     addon.d 16.5.20180402     "
 	ui_print "       created by @Nanolx      "
 	ui_print " Utilizing DexPatcher @lanchon "
 	ui_print "*******************************"
