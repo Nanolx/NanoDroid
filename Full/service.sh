0bbdb05262ea03469178c89091280769f2752484
Christopher Roy Bratusek <nano@jpberlin.de>
2019-05-05 20:22:30 +0200

    Full/service.sh: move init.d logs and simplify code

--- Full/service.sh
+++ Full/service.sh
@@ -4,19 +4,19 @@
 MODDIR=${0%/*}
 
 # Create private Log directory
-LOGDIR="${MODDIR}"/.logs
+LOGDIR="${MODDIR}"/logs
 
 [[ ! -d ${LOGDIR} ]] && mkdir -p "${LOGDIR}"
-
-# Set current date in 20170607-12.07.25 format
-CURDATE=$(date +%Y%m%d-%I.%M.%S)
 
 # wait until boot completed
 until [ `getprop sys.boot_completed`. = 1. ]; do sleep 1; done
 
+# Set current date in 20170607-12.07.25 format
+CURDATE=$(date +%Y%m%d-%I.%M.%S)
+
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
-for init in fstrim logscleaner sqlite external_sd; do
+for init in fstrim logscleaner sqlite external_sd logcat; do
 	"${MODDIR}/init.d/${init}" | \
 		tee -a "${LOGDIR}/${init}.log.${CURDATE}" &
 done
@@ -29,5 +29,3 @@ pm list packages -f | grep -q /data.*org.microg.gms.droidguard || \
 [ -f /data/adb/magisk.apk ] && \
 	pm install -r /data/adb/magisk.apk ; \
 	rm -f /data/adb/magisk.apk &
-
-"${MODDIR}/init.d/logcat" &
